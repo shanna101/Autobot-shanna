5453d270b84ef422af2b60b7c1d249fd920749e5
scyte000 <scyte000@gmail.com>
2024-05-12 13:08:10 +0800

    Update info.js

--- script/info.js
+++ script/info.js
@@ -24,10 +24,10 @@ module.exports.run = async function({ api, event, args }) {
 		let seconds = Math.floor(time % 60);
 		const uptimeString = `${years > 0 ? `${years} years ` : ''}${months > 0 ? `${months} months ` : ''}${weeks > 0 ? `${weeks} weeks ` : ''}${days % 7 > 0 ? `${days % 7} days ` : ''}${hours > 0 ? `${hours} hours ` : ''}${minutes > 0 ? `${minutes} minutes ` : ''}${seconds} seconds`;
 
-		const prefix = "!";
-		const CREATORLINK = "https://www.facebook.com/marjhxn";
-		const BOTCREATOR = "Marjhxn";
-		const BOTNAME = "Astral";
+		const prefix = "+";
+		const CREATORLINK = "https://www.facebook.com/61550264923277";
+		const BOTCREATOR = "AJ CHICANO";
+		const BOTNAME = "BOTIBOT";
 		const FILESOWNER = "";
 		const juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 		const link = ["https://i.imgur.com/9LDVC57.mp4", "https://i.imgur.com/r7IxgiR.mp4", "https://i.imgur.com/J1jWubu.mp4", "https://i.imgur.com/DJylTiy.mp4", "https://i.imgur.com/v4mLGte.mp4", "https://i.imgur.com/uthREbe.mp4", "https://i.imgur.com/ee8fHna.mp4", "https://i.imgur.com/VffzOwS.mp4", "https://i.imgur.com/ci5nztg.mp4", "https://i.imgur.com/qHPeKDV.mp4", "https://i.imgur.com/Rkl5UmH.mp4"];
